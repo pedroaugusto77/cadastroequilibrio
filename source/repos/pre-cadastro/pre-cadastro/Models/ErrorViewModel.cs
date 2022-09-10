14ce588a387e7e83b92c3443ec8664623615b211
pedroaugusto77 <pedroaugustogla1995@gmail.com>
2022-09-10 17:13:41 -0300

    primarycommit

---
+++ source/repos/pre-cadastro/pre-cadastro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pre_cadastro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
